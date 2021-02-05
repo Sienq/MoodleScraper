b8446d92aee1adadbe49a9bab33ea61996c71c75
Sienq <Hubert.Sienkiewicz99@gmail.com>
2021-02-05 03:55:36 +0100

    24h alert

--- sendMail.py
+++ sendMail.py
@@ -3,6 +3,8 @@ import scrapData
 import get_credentials
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import datetime
+import calendar_event
 
 
 def email(events,address):
@@ -13,13 +15,11 @@ def email(events,address):
     server.login('moodlemajor@gmail.com','japodatkiplace')
 
     sender = "MoodleMajor@gmail.com"
-
     msg = MIMEMultipart()
 
     msg['From'] = sender
-    msg['To'] = addr
+    msg['To'] = address
     msg['Subject'] = "NEW ASSIGNMENT"
-    subject = "NEW ASSIGNMENT"
 
     body = ""
     for event in events:
@@ -27,6 +27,31 @@ def email(events,address):
 
     msg.attach(MIMEText(body,'plain'))
     emailText = msg.as_string()    
-    server.sendmail(sender, addr, emailText)
+    server.sendmail(sender, address, emailText)
     server.quit()
+
+def alert(event,timeleft,address):
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.login('moodlemajor@gmail.com','japodatkiplace')
+    sender = "MoodleMajor@gmail.com"
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = address
+    msg['Subject'] = "24H ALERT"
+    body = "Time left for: " + event.name + " is " + timeleft
+    msg.attach(MIMEText(body,'plain')) 
+
+    emailText = msg.as_string()    
+    server.sendmail(sender, address, emailText)
+    server.quit()
+
+def checkTime(address):
+    print("siema")
+    now = datetime.datetime.now()
+    events = calendar_event.loadEvents('events.json')
+    for event in events:
+        if event.date - now < datetime.timedelta(hours=24):
+            alert(event,str(event.date-now),address)        
 

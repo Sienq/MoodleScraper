0958ee912c4d998cc9e560d4cc6172f14ad0995a
Adam Pindelski <adampindelski@Adams-MacBook-Pro-2.local>
2021-02-05 03:25:17 +0100

    beta 1.0.

--- get_credentials.py
+++ get_credentials.py
@@ -4,7 +4,7 @@ def getCredentials(file_name):
     with open(file_name, "r") as f:
         
         credentials = f.readlines()
-        nick = cridentials[0].strip()
+        nick = credentials[0].strip()
         password = cridentials[1].strip()
 
         return nick, password

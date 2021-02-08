9c8605dbb6405949310327dac2a58cc577a22fb3
Sienq <Hubert.Sienkiewicz99@gmail.com>
2021-02-08 22:19:48 +0100

    on_message full_functionality

--- dscBot.py
+++ dscBot.py
@@ -6,14 +6,19 @@ from discord import message
 from dotenv import load_dotenv
 import calendarEvent
 import asyncio
+import periodicScraper
+from sys import argv
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
+DATE_FORMAT = '%Y-%m-%d %H:%M'
+
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connectedn to Discord')
+    print(f'{client.user} has connected to Discord')
 
 @client.event
 async def on_message(message):
@@ -21,10 +26,11 @@ async def on_message(message):
         return
     if message.content.startswith("!Tasks"): #!MESSAGE AUTHOR IN USERLIST
         user = message.author
+        events = calendarEvent.loadEvents('hubert_events.json')
         #!LOAD USER TASKS
-        # for event in events:
-        #     await user.send(event.name + " " + event.course + " " + event.date)
-        # await asyncio.sleep(10)
+        for event in events:
+            await user.send(event.name + " " + event.course + " " + str(event.date))
+        await asyncio.sleep(10)
 
 async def update(events,userID):
     await client.wait_until_ready()
@@ -35,4 +41,9 @@ async def update(events,userID):
 
 # events = [calendarEvent.CalendarEvent("pierwszy","jakis","25-02-2020"),calendarEvent.CalendarEvent("drugi","jakis","22-02-2020")]
 # client.loop.create_task(update(events,302525505783988226))
+
+
 client.run(TOKEN)
+user_list_file_name = argv[1]
+users = periodicScraper.loadUserList(user_list_file_name)
+    
